5e7242c5fa82ea66e915601b3eaa185e9ffc3449
Guillaume Clément <gclement@baobob.org>
2012-12-18 13:55:11 +0100

    Annulation de l'utilisation du hostname long

--- rc.lua
+++ rc.lua
@@ -33,7 +33,7 @@ end
 -- }}}
 
 function hostname()
-   local f = io.popen ("/bin/hostname -f")
+   local f = io.popen ("/bin/hostname")
    local n = f:read("*a") or "none"
    f:close()
    n=string.gsub(n, "\n$", "")
