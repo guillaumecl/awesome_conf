994494470572805cf47a99eece7e3383e0d5f2ea
Guillaume Clément <gclement@baobob.org>
2013-04-28 16:59:14 +0200

    Gestion des touches audio dans awesome

--- heimdall.lua
+++ heimdall.lua
@@ -1,1 +1,16 @@
-
+-- {{{ Key bindings
+globalkeys = awful.util.table.join(
+   globalkeys,
+   awful.key({ }, "XF86AudioLowerVolume", function ()
+											 awful.util.spawn("amixer -M -c 1 -q sset PCM 2-")
+											 awful.util.spawn("amixer -M -c 0 -q sset Master 2-")
+										  end),
+   awful.key({ }, "XF86AudioRaiseVolume", function ()
+											 awful.util.spawn("amixer -M -c 1 -q sset PCM 2+")
+											 awful.util.spawn("amixer -M -c 0 -q sset Master 2+")
+										  end),
+   awful.key({ }, "XF86AudioMute", function ()
+											 awful.util.spawn("amixer -M -c 1 -q sset PCM toggle")
+											 awful.util.spawn("amixer -M -c 0 -q sset Master toggle")
+										  end)
+)
