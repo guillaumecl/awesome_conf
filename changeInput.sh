e09d2bf46da115f2407239282d0e32ddb608d0ad
Guillaume Clément <gclement@baobob.org>
2013-04-29 20:14:27 +0200

    Plus de lignes dans les keyboards

--- changeInput.sh
+++ changeInput.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-deviceId=$(xinput list | grep -A5 "Virtual core keyboard" | egrep 'USB|AT' | grep -v TypeMatrix | sed 's/.*id=\([0-9]*\).*/\1/')
+deviceId=$(xinput list | grep -A9 "Virtual core keyboard" | egrep 'USB|AT' | grep -v TypeMatrix | sed 's/.*id=\([0-9]*\).*/\1/')
 
 setxkbmap -device $deviceId $@
