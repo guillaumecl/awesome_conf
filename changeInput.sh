f281500e614d3e41d302c356e1537cabdb6df986
Guillaume Clément <gclement@baobob.org>
2013-05-19 17:51:06 +0200

    Gestion du cas où on a plusieurs claviers (ex: portable)

--- changeInput.sh
+++ changeInput.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
-deviceId=$(xinput list | grep -A9 "Virtual core keyboard" | egrep 'USB|AT' | grep -v TypeMatrix | sed 's/.*id=\([0-9]*\).*/\1/')
+deviceIds=$(xinput list | grep -A9 "Virtual core keyboard" | egrep 'USB|AT' | grep -v TypeMatrix | sed 's/.*id=\([0-9]*\).*/\1/')
 
-setxkbmap -device $deviceId $@
+for deviceId in $deviceIds
+do
+	setxkbmap -device $deviceId $@
+done
+
