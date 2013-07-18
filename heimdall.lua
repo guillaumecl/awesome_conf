69846c07173333626cf85a147ae11dc7c8d981d2
Guillaume Clément <gclement@baobob.org>
2013-07-18 18:47:50 +0200

    Modif de l'endroit où est stocké tmpc

--- heimdall.lua
+++ heimdall.lua
@@ -14,9 +14,9 @@ globalkeys = awful.util.table.join(
 									  awful.util.spawn("amixer -M -c 0 -q sset Master toggle")
 								   end),
    awful.key({ modkey,      }, "y", function ()
-								 awful.util.spawn("/home/tortuxm/projets/tmpc/build/tmpc")
+								 awful.util.spawn("/home/tortuxm/projets/build/tmpc/tmpc")
 							  end),
    awful.key({ modkey,      }, "c", function ()
-								 awful.util.spawn("/home/tortuxm/projets/tmpc/build/tmpc --current")
+								 awful.util.spawn("/home/tortuxm/projets/build/tmpc/tmpc --current")
 							  end)
 )
