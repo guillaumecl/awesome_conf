21e1f0ff976b2d1f00c5a5038a2023ad1edd9ec2
Guillaume Clément <gclement@baobob.org>
2013-09-24 14:46:35 +0200

    Reindentation + run_or_raise pour tmpc

--- thor.lua
+++ thor.lua
@@ -8,23 +8,23 @@ customtags = true
 globalkeys = awful.util.table.join(
    globalkeys,
    awful.key({ }, "XF86AudioLowerVolume", function ()
-											 awful.util.spawn("amixer -M -c 0 -q sset Master 2-")
-										  end),
+				awful.util.spawn("amixer -M -c 0 -q sset Master 2-")
+   end),
    awful.key({ }, "XF86AudioRaiseVolume", function ()
-											 awful.util.spawn("amixer -M -c 0 -q sset Master 2+")
-										  end),
+				awful.util.spawn("amixer -M -c 0 -q sset Master 2+")
+   end),
    awful.key({ }, "XF86AudioMute", function ()
-									  awful.util.spawn("amixer -M -c 0 -q sset Master toggle")
-								   end),
+				awful.util.spawn("amixer -M -c 0 -q sset Master toggle")
+   end),
    awful.key({ }, "XF86Tools", function ()
-									  awful.util.spawn(base .. "/toggle_matchbox.sh")
-								   end),
+				awful.util.spawn(base .. "/toggle_matchbox.sh")
+   end),
    awful.key({ modkey,      }, "y", function ()
-								 awful.util.spawn("/home/tortuxm/projets/build/tmpc/tmpc")
-							  end),
+				run_or_raise("/home/tortuxm/projets/build/tmpc/tmpc", { instance = "tmpc" } )
+   end),
    awful.key({ modkey,      }, "c", function ()
-								 awful.util.spawn("/home/tortuxm/projets/build/tmpc/tmpc --current")
-							  end)
+				awful.util.spawn("/home/tortuxm/projets/build/tmpc/tmpc --current")
+   end)
 )
 
 browser = "qupzilla -nw"
