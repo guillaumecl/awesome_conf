481bc628c9fe5b811bca61222929870a4a2c9642
Guillaume Clement <gclement@baobob.org>
2013-10-01 21:05:33 +0200

    Suppression d'un warning

--- rc.lua
+++ rc.lua
@@ -341,8 +341,8 @@ globalkeys = awful.util.table.join(
    awful.key({ modkey            }, "e",function () run_or_raise(editor_cmd, { instance = "emacs" } ) end),
    awful.key({ modkey, "Shift"   }, "e",function () awful.util.spawn(editor_cmd ) end),
 
-   awful.key({ modkey            }, "a",function () run_or_raise(terminal .. " -t emacs -e emacs -nw", { name = "emacs", instance="urxvt" } ) end),
-   awful.key({ modkey, "Shift"   }, "a",function () awful.util.spawn(terminal .. " -t emacs -e emacs -nw" ) end),
+   awful.key({ modkey            }, "a",function () run_or_raise(terminal .. " -e emacs -nw", { name = "emacs", instance="urxvt" } ) end),
+   awful.key({ modkey, "Shift"   }, "a",function () awful.util.spawn(terminal .. " -e emacs -nw" ) end),
 
    awful.key({ modkey            }, "d",function () run_or_raise(filer, filer_instance ) end),
    awful.key({ modkey, "Shift"   }, "d",function () awful.util.spawn(filer ) end),
