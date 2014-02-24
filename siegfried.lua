f0123c65988af9eb226befaf85e7d0c75c6d53c6
Guillaume Clément <gclement@baobob.org>
2014-02-24 20:08:22 +0100

    Mises à jour diverses

--- siegfried.lua
+++ siegfried.lua
@@ -1,5 +1,5 @@
 
-terminal = "urxvt"
+terminal = "urxvtc"
 customtags = true
 
 for s = 1, screen.count() do
@@ -9,16 +9,48 @@ for s = 1, screen.count() do
    -- "➓"
 end
 
-browser = "qupzilla -nw"
-browser_instance = { instance = "qupzilla" }
+
+globalkeys = awful.util.table.join(
+   globalkeys,
+
+   awful.key({ "Control"}, "XF86AudioLowerVolume", function ()
+				awful.util.spawn("ssh odin amixer -M -c 0 -q sset Master 2-")
+   end),
+   awful.key({ "Control" }, "XF86AudioRaiseVolume", function ()
+				awful.util.spawn("ssh odin amixer -M -c 0 -q sset Master 2+")
+   end),
+   awful.key({ "Control" }, "XF86AudioMute", function ()
+				awful.util.spawn("ssh odin amixer -M -c 0 -q sset Master toggle")
+   end),
+
+   awful.key({ modkey, "Control" }, "Return", function ()
+				awful.util.spawn("ssh odin mpc toggle")
+   end),
+   awful.key({ modkey, "Control" }, "Right", function ()
+				awful.util.spawn("ssh odin mpc next")
+   end),
+   awful.key({ modkey, "Control" }, "Left", function ()
+				awful.util.spawn("ssh odin mpc prev")
+   end),
+   awful.key({ modkey, "Control"}, "Up", function ()
+				awful.util.spawn("ssh odin amixer -M -c 0 -q sset Master 2dB+")
+   end),
+   awful.key({ modkey, "Control" }, "Down", function ()
+				awful.util.spawn("ssh odin amixer -M -c 0 -q sset Master 2dB-")
+   end),
+
+   awful.key({ modkey,      }, "y", function ()
+				run_or_raise("bash -c 'cd /home/tortuxm/projets/tmpc/build; ./tmpc'", { instance = "tmpc" } )
+   end),
+   awful.key({ modkey, "Control" }, "y", function ()
+				run_or_raise("bash -c 'cd /home/tortuxm/projets/tmpc/build; MPD_HOST=odin ./tmpc'", { instance = "tmpc" } )
+   end),
+   awful.key({ modkey,      }, "c", function ()
+				awful.util.spawn("bash -c 'cd /home/tortuxm/projets/tmpc/build; ./tmpc --current'")
+   end),
+   awful.key({ modkey, "Control" }, "c", function ()
+				awful.util.spawn("bash -c 'cd /home/tortuxm/projets/tmpc/build; MPD_HOST=odin ./tmpc --current'")
+   end)
+)
 
 
-
-rules = {
-   { rule = { instance = "emacs" },
-     properties = { tag = tags[1][1] } },
-   { rule = { instance = "ktorrent" },
-     properties = { tag = tags[1][9] } },
-   { rule = { instance = "cantata" },
-     properties = { tag = tags[1][8] } }
-}
